2b5c10219fa2e1d84ef27363606e98eb5d99a54f
pedro <pedro.aarm@gmail.com>
2019-10-27 23:38:45 -0300

    Melhoria nas telas de upload e de listar artigos
    Praticamente concluido

--- esppedu/revista/models.py
+++ esppedu/revista/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+
 #D'Larinhoo mexendo aqui
 
 from django.contrib.auth.models import User
@@ -7,25 +9,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 #------------------------------------------
-    
-class Artigo(models.Model):
-
-    cod_artigo = models.AutoField(primary_key=True)
-    titulo = models.CharField(max_length=30, null=False, blank=False)
-    resumo = models.CharField(max_length=3000, null=False, blank=False)
-    palavras_chaves = models.CharField(max_length=200, null=False, blank=False)
-    area = models.CharField(max_length=100, null=False, blank=False)
-    situacao = models.CharField(max_length=30, null=False, blank=False)
-    pdf = models.FileField(upload_to='artigos/pdfs')
-    autor = models.CharField(max_length=70, null=False, blank=False)
-
-    def __str__(self):
-        return self.titulo
-
-    def deletar(self, *args, **kwargs):
-        self.pdf.deletar()
-        super().deletar(*args, **kwargs)
-
 
 # Create your models here.
 
@@ -44,3 +27,29 @@ def update_profile_signal(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
+
+
+class Artigo(models.Model):
+
+
+   
+
+    autor = models.CharField(max_length=30, null=False, blank=False)
+    cod_artigo = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=30, null=False, blank=False)
+    resumo = models.CharField(max_length=3000, null=False, blank=False)
+    palavras_chaves = models.CharField(max_length=200, null=False, blank=False)
+    area = models.CharField(max_length=100, null=False, blank=False)
+    situacao = models.CharField(max_length=30, null=False, blank=False)
+    pdf = models.FileField(upload_to='artigos/')
+
+
+    def __str__(self):
+        return self.titulo
+
+    def deletar(self, *args, **kwargs):
+        self.pdf.deletar()
+        super().deletar(*args, **kwargs)
+
+
+
